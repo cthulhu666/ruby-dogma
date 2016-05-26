327d9fce4f941fee514de454048b7f639e6aff95
Jakub Głuszecki <jakub.gluszecki@gmail.com>
2016-05-26 15:40:24 +0200

    Refactor specs a little bit

--- spec/dogma_spec.rb
+++ spec/dogma_spec.rb
@@ -9,35 +9,38 @@ RSpec.describe 'Dogma' do
       ctx.set_ship 587 # Rifter
     end
 
-    context 'without any modules' do
-      it 'has proper shield capacity' do
-        shield_capacity = ctx.ship_attribute 263
-        expect(shield_capacity).to eq 562.5
+    describe 'shield capacity' do
+      let(:shield_capacity) { ctx.ship_attribute 263 }
+      subject { shield_capacity }
+
+      context 'without any modules' do
+        it { is_expected.to eq 562.5 }
+      end
+
+      context 'with shield extender' do
+        before { ctx.add_module 3831 } # Medium Shield Extender II
+
+        it {   is_expected.to eq 1937.5 }
       end
     end
 
-    context 'with shield extender' do
-      before { ctx.add_module 3831 } # Medium Shield Extender II
-
-      it 'has proper shield capacity' do
-        shield_capacity = ctx.ship_attribute 263
-        expect(shield_capacity).to eq 1937.5
+    describe 'falloff' do
+      let(:falloffs) do
+        idx.map { |i| ctx.module_attribute 158, i } # ATT_Falloff
       end
-    end
+      subject { falloffs }
 
-    context 'with autocannons' do
-      let(:idx) do
-        [
-          # TYPE_150mmLightAutoCannonII + TYPE_ArchAngelPhasedPlasmaS
-          ctx.add_module(2881, charge: 19_988, state: Dogma::STATE_ACTIVE),
-          # TYPE_200mmLightAutoCannonII + TYPE_ArchAngelPhasedPlasmaS
-          ctx.add_module(2889, charge: 19_988, state: Dogma::STATE_ACTIVE)
-        ]
-      end
+      context 'with autocannons' do
+        let(:idx) do
+          [
+            # TYPE_150mmLightAutoCannonII + TYPE_ArchAngelPhasedPlasmaS
+            ctx.add_module(2881, charge: 19_988, state: Dogma::STATE_ACTIVE),
+            # TYPE_200mmLightAutoCannonII + TYPE_ArchAngelPhasedPlasmaS
+            ctx.add_module(2889, charge: 19_988, state: Dogma::STATE_ACTIVE)
+          ]
+        end
 
-      it 'has proper attributes' do
-        falloffs = idx.map { |i| ctx.module_attribute 158, i } # ATT_Falloff
-        expect(falloffs).to eq [8868.75, 9675.00]
+        it { is_expected.to eq [8868.75, 9675.00] }
       end
     end
   end
